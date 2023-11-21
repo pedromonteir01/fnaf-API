aaa10c1fb9aca08d0d1a53ce7f98e8170ee576c8
Pedro Monteiro <pedromonteiro.senai564@gmail.com>
2023-11-21 13:51:18 -0300

    feat: animatronic router

--- src/router/animatronic.routes.js
+++ src/router/animatronic.routes.js
@@ -1,5 +1,18 @@
 import { Router } from "express";
+import { 
+    deleteAnimatronic, 
+    getAnimatronicById, 
+    getAnimatronics, 
+    postAnimtronic, 
+    putAnimatronic
+} from "../controller/animatronics.controller";
 
 const animatronicRouter = Router();
+
+animatronicRouter.get("/", getAnimatronics);
+animatronicRouter.get("/:id", getAnimatronicById);
+animatronicRouter.post("/", postAnimtronic);
+animatronicRouter.put("/:id", putAnimatronic);
+animatronicRouter.delete("/:id", deleteAnimatronic);
 
 export default animatronicRouter;
