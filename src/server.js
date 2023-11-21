8c66934aec75f459161b16d4ecedf6cc8609a6a0
Pedro Monteiro <pedromonteiro.senai564@gmail.com>
2023-11-21 10:17:14 -0300

    feat: all components server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
+import router from "./router/index.routes.js";
 
 config();
 
@@ -7,10 +8,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-
-app.get("/", (req, res) => {
-    return res.status(200).send({message: "SERVER OK!"});
-}) 
+app.use(router);
 
 app.listen(port, () => {
     console.log(`⚡ Server started on http://localhost:${port}`);
