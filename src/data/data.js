76cced286418aad9ea6851fbee0302248b19a78a
Daniel <danielsantos.senai564@gmail.com>
2023-12-05 11:03:37 -0300

    registrations animatronics

--- src/data/data.js
+++ src/data/data.js
@@ -1,27 +1,313 @@
 export const mockedAnimatronic = [
     {
         "name": "Freddy FazBear",
-        "imageBody": "#",
-        "imageIcon": "../assets/images/FreddyFazbear.png", 
+        "imageBody": "../assets/images/FreddyFazbear-icon.png",
+        "imageIcon": "../assets/images/FreddyFazbear.png",
         "occupation": "Freddy Fazbear's Pizza (1993)",
-        "initialLocation": "Geralmente, Freddy Fazbear começa sua noite na Sala de Show Principal, mas ao longo da noite, ele se move para outras áreas da pizzaria, como o Corredor Leste e o Corredor Oeste.", 
-        "description": "Freddy Fazbear é um animatrônico gigante e principal personagem da franquia 'Five Nights at Freddy's'. Ele é um urso marrom com uma cartola preta, uma gravata borboleta preta e botões pretos no peito. Freddy é conhecido por sua aparência amigável, mas se torna perigoso à noite quando os animatrônicos ficam ativos.", 
-        "color": "marrom", 
-        "status": "Inteiro, porém com a parte da mão direita quebrada. Durante o jogo, Freddy Fazbear pode se tornar mais ativo e agressivo, especialmente nas noites mais avançadas.", 
-        "instrument": "Em algumas aparições, Freddy Fazbear é retratado segurando um microfone, indicando seu papel como animatrônico de entretenimento na pizzaria.", 
+        "initialLocation": "Geralmente, Freddy Fazbear começa sua noite na Sala de Show Principal, mas ao longo da noite, ele se move para outras áreas da pizzaria, como o Corredor Leste e o Corredor Oeste.",
+        "description": "Freddy Fazbear é um animatrônico gigante e principal personagem da franquia 'Five Nights at Freddy's'. Ele é um urso marrom com uma cartola preta, uma gravata borboleta preta e botões pretos no peito. Freddy é conhecido por sua aparência amigável, mas se torna perigoso à noite quando os animatrônicos ficam ativos.",
+        "color": "marrom",
+        "status": "Inteiro, porém com a parte da mão direita quebrada. Durante o jogo, Freddy Fazbear pode se tornar mais ativo e agressivo, especialmente nas noites mais avançadas.",
+        "instrument": "Em algumas aparições, Freddy Fazbear é retratado segurando um microfone, indicando seu papel como animatrônico de entretenimento na pizzaria.",
         "jumpscare": "../assets/videos/jump-Scare-Freddy-Fazbear.mp4"
+    },
+
+    {
+        "name": "Bonnie the Bunny",
+        "imageBody": "../assets/images/Bonnie-icon.png",
+        "imageIcon": "../assets/images/Bonnie.png",
+        "occupation": "Freddy Fazbear's Pizza (1993)",
+        "initialLocation": "Bonnie geralmente começa a noite na Sala de Show Principal ou em áreas próximas, mas como outros animatrônicos, ele se move para diferentes partes da pizzaria ao longo da noite.",
+        "description": "Bonnie é um coelho animatrônico com pele azul, olhos grandes e uma guitarra elétrica. Ele é conhecido por sua natureza agressiva à noite, perseguindo o jogador pelo ambiente da pizzaria.",
+        "color": "Azul",
+        "status": "Bonnie é ativo durante as noites nos jogos da série e se torna mais agressivo à medida que as noites avançam. Como muitos dos animatrônicos na franquia, ele pode ser representado como quebrado ou deteriorado, especialmente em versões mais avançadas da série.",
+        "instrument": "Bonnie é retratado segurando uma guitarra elétrica, sugerindo seu papel como animatrônico de entretenimento na pizzaria.",
+        "jumpscare": "../assets/videos/jump-Scare-Bonnie.mp4"
+    },
+
+    {
+        "name": "Chica the Chicken",
+        "imageBody": "../assets/images/Chica-icon.png",
+        "imageIcon": "../assets/images/Chica.png",
+        "occupation": "Freddy Fazbear's Pizza (1993)",
+        "initialLocation": "Chica geralmente começa a noite na Sala de Show Principal ou em áreas próximas, mas durante a noite, ela se movimenta por diferentes partes da pizzaria em busca do jogador.",
+        "description": " Chica é um animatrônico em forma de galinha com penas amarelas, um bico laranja e olhos grandes. Ela é um dos personagens principais da banda animatrônica da pizzaria e é conhecida por sua natureza agressiva durante a noite.",
+        "color": "Amarelo",
+        "status": "Chica é ativa durante as noites nos jogos da série e se torna mais agressiva à medida que as noites avançam. Como muitos dos animatrônicos na franquia, ela pode ser representada como quebrada ou deteriorada, especialmente em versões mais avançadas da série.",
+        "instrument": "Chica é retratada segurando uma bandeja de pizza, sugerindo seu papel como animatrônico de entretenimento na pizzaria.",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "Foxy the Pirate Fox",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "Freddy Fazbear's Pizza (1993)",
+        "initialLocation": "Foxy começa suas noites na Sala de Piratas ou na Câmara de Segurança, dependendo do jogo específico. Ele é conhecido por ser mais ativo ao longo da noite, correndo em direção ao jogador.",
+        "description": " Foxy é um animatrônico em forma de raposa com pele vermelha, olhos amarelos e uma pata de gancho. Ele veste uma faixa preta e uma bermuda rasgada, dando-lhe uma aparência de pirata. Foxy é notável por sua natureza agressiva e por se mover rapidamente em direção ao jogador.",
+        "color": "Vermelho",
+        "status": "Em alguns jogos da franquia, Foxy pode ser representado como quebrado ou desgastado devido ao uso constante. Sua condição pode variar, mas geralmente ele é retratado como um animatrônico mais deteriorado.",
+        "instrument": "Foxy não é associado a um instrumento musical específico. Sua ênfase está mais na temática de piratas, com seu gancho e seu comportamento agressivo.",
+        "jumpscare": ""
     },
 
     {
         "name": "",
         "imageBody": "",
-        "imageIcon": "", 
+        "imageIcon": "",
         "occupation": "",
-        "initialLocation": "", 
-        "description": "", 
-        "color": "", 
-        "status": "", 
-        "instrument": "", 
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
         "jumpscare": ""
-    }
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
+
+    {
+        "name": "",
+        "imageBody": "",
+        "imageIcon": "",
+        "occupation": "",
+        "initialLocation": "",
+        "description": "",
+        "color": "",
+        "status": "",
+        "instrument": "",
+        "jumpscare": ""
+    },
 ]
