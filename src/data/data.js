16e93f8da42dbac994d29e4e65b355be31f4df05
Daniel <danielsantos.senai564@gmail.com>
2023-12-05 16:14:57 -0300

    registrations Rockstar Bonnie

--- src/data/data.js
+++ src/data/data.js
@@ -559,15 +559,15 @@ export const mockedAnimatronic = [
     },
 
     {
-        "name": "",
+        "name": "Rockstar Bonnie",
         "imageBody": "",
         "imageIcon": "",
-        "occupation": "",
-        "initialLocation": "",
-        "description": "",
-        "color": "",
-        "status": "",
-        "instrument": "",
+        "occupation": "Rockstar Bonnie está associado à pizzaria fictícia Freddy Fazbear's Pizza, introduzida na série Five Nights at Freddy's. Ele faz parte do grupo de animatrônicos conhecido como Rockstar Animatronics.",
+        "initialLocation": "Rockstar Bonnie pode ser encontrado em diferentes áreas da Freddy Fazbear's Pizza, dependendo do jogo específico. Sua presença é notável em contextos nos quais ele é um antagonista significativo.",
+        "description": "Rockstar Bonnie é um animatrônico coelho com uma aparência moderna e refinada. Ele é caracterizado por seus traços mais robustos e pela presença de uma estrela em seu peito. Rockstar Bonnie desempenha um papel específico na narrativa e nas mecânicas de jogo.",
+        "color": "As cores principais de Rockstar Bonnie incluem tons de roxo, azul e preto, destacando-se por sua aparência mais atualizada e polida.",
+        "status": "Em termos de narrativa, Rockstar Bonnie não é retratado como quebrado ou danificado. Ele representa uma versão modernizada dos animatrônicos originais.",
+        "instrument": "Rockstar Bonnie não está associado a um instrumento musical específico.",
         "jumpscare": ""
     },
 
